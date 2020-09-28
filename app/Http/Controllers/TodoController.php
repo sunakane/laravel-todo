3958ea7db40007509b36b8f3db1669ede0d0820a
sunakane <gedyra@gmail.com>
2020-09-28 14:41:26 +0900

    create

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TodoController extends Controller
+{
+    public function create(Request $request) {
+        DB::table('todos')->insert([
+            'title' => $request['title']
+        ]);
+        return response()->json(['ok']);
+    }
+}
