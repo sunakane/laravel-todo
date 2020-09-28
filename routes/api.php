c7ddb923be13777178e4358aa5d92fb42e08b405
sunakane <gedyra@gmail.com>
2020-09-28 13:52:20 +0900

    get -> post

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => ['api']], function () {
-    Route::get('test', 'TestController@test');
+    Route::post('test', 'TestController@test');
 });
