470803588b341121ba0aeba89417d4269861f776
sunakane <gedyra@gmail.com>
2020-09-28 14:02:34 +0900

    simplified

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['api']], function () {
-    Route::post('test', 'TestController@test');
-});
+Route::post('test', 'TestController@test');
